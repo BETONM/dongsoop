fd47f10b727aec9588f19038c12e975b4a30753f
dltpgh6021 <seholee7003@gmail.com>
2026-08-05 16:49:48 +0900

    feat: 현재 수행해야 하는 미션 출력

--- api/mission.js
+++ api/mission.js
@@ -1,15 +1,124 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
-const { OAuth2Client } = require('google-auth-library');
-const jwt = require('jsonwebtoken');
 
-router.get('/', (req, res) => {
-    res.json({ success: true, message: "mission api 성공" });
-});
+router.get('/', async (req, res) => {
+    const google_id = req.user.google_id;
 
-router.get('/', (req, res) => {
-    
+    try {
+        const searchQuery = `
+            SELECT completed_mission
+            FROM USERS
+            WHERE google_id = $1
+        `;
+
+        const searchResult = await db.query(searchQuery, [ google_id ]);
+
+        const user = searchResult.rows[0];
+
+        if (!user) {
+            return res.status(404).json({
+                success: false, 
+                message: "사용자 없음"
+            });
+        }
+
+        const completedMission = user.completed_mission;
+
+        switch (completedMission) {
+            case 0: 
+                const m1Query = `
+                    SELECT *
+                    FROM mission1
+                    WHERE google_id = $1
+                `;
+                const m1Result = await db.query(m1Query, [ google_id ]);
+                const mission1 = m1Result.rows[0];
+
+                if (!mission1) {
+                    return res.status(404).json({
+                        success: false, 
+                        message: "Mission1이 없습니다"
+                    });
+                }
+
+                return res.status(200).json({
+                    success: true, 
+                    message: "주민과 3분간 대화하기", 
+                    mission: {
+                        number: 1, 
+                        target_char: mission1.target_char, 
+                        completed: mission1.completed
+                    }
+                });
+            case 1:
+                const m2Query = `
+                    SELECT *
+                    FROM mission2
+                    WHERE google_id = $1
+                `;
+                const m2Result = await db.query(m2Query, [ google_id ]);
+                const mission2 = m2Result.rows[0];
+
+                if (!mission2) {
+                    return res.status(404).json({
+                        success: false, 
+                        message: "Mission2이 없습니다"
+                    });
+                }
+
+                return res.status(200).json({
+                    success: true, 
+                    message: "주민과 사진 미션 수행하기", 
+                    mission: {
+                        number: 2, 
+                        target_char: mission2.target_char, 
+                        completed: mission2.completed
+                    }
+                });
+            case 2:
+                const m3Query = `
+                    SELECT *
+                    FROM mission3
+                    WHERE google_id = $1
+                `;
+                const m3Result = await db.query(m3Query, [ google_id ]);
+                const mission3 = m3Result.rows[0];
+
+                if (!mission3) {
+                    return res.status(404).json({
+                        success: false, 
+                        message: "주민과 뱃지 인증사진 찍기"
+                    });
+                }
+
+                return res.status(200).json({
+                    success: true, 
+                    message: "주민과 프메 스티커 인증사진 찍기", 
+                    mission: {
+                        number: 3, 
+                        completed: mission3.completed
+                    }
+                });
+            case 3: 
+                return res.status(200).json({
+                    success: true, 
+                    message: "모든 미션을 완료했습니다. "
+                });
+            default: 
+                return res.status(400).json({
+                    success: false,
+                    message: "잘못된 미션 상태입니다. "
+                });
+        }
+    }
+    catch (err) {
+        console.error("미션 탐색 오류: ", err);
+        res.status(500).json({
+            success: false, 
+            message: "서버 오류 발생"
+        });
+    }
 });
 
 module.exports = router;
--- docs/database.md
+++ docs/database.md
@@ -1,8 +1,8 @@
 ```js
 USERS {
     string google_id PK NOT NULL // 구글 아이디
-    string name NOT NULL // 사용자 이름
-    string char_name NOT NULL // 동숲 캐릭터 이름
+    string name // 사용자 이름
+    string char_name // 동숲 캐릭터 이름
     int completed_mission DEFAULT 0 
     /*
     미션 클리어 개수 
@@ -10,14 +10,15 @@ USERS {
     1개: 1, 
     2개: 2, 
     3개: 3, 
-    개발자 코드 입력 완료: 4
     */
+    boolean developer_verified DEFAULT false // 관리자 비밀번호 입력 완료 시
 }
 
 MISSION1 {
     string google_id PK FK NOT NULL // 구글 아이디
     string target_char NOT NULL // 미션1 대상 캐릭터
     boolean completed DEFAULT FALSE // 미션1 성공 여부
+    timestamp created_at // 만들어진 시간
 }
 
 MISSION2 {
@@ -26,13 +27,14 @@ MISSION2 {
     string mission NOT NULL // 미션 내용
     BYTEA image // 미션2 성공 이미지
     boolean completed DEFAULT FALSE // 미션2 성공 여부
+    timestamp created_at // 만들어진 시간
 }
 
 MISSION3 {
     string google_id PK FK NOT NULL // 구글 아이디
-    string comment // 멘트
     BYTEA image // 미션3 성공 이미지
     boolean completed DEFAULT FALSE // 미션3 성공 여부
+    timestamp created_at // 만들어진 시간
 }
 
 ```
